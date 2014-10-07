7651bfa64cfe700d6901465c9c203533f68f5567
Matt Parsons <parsonsmatt@gmail.com>
2014-10-07 03:06:33 -0400

    Initial wrapping complete

---
+++ README.md
@@ -0,0 +1,5 @@
+# ngWifiWizard
+
+This project wraps the WifiWizard Cordova plugin for use with AngularJS and Ionic Framework
+
+Currently working with WifiWizard v0.2.3.
--- wifiwizard.js
+++ wifiwizard.js
@@ -8,43 +8,122 @@
 		.factory('$cordovaWifiWizard', [ '$q', '$window', function ($q, $window) {
 			return {
 				formatWifiConfig: function(SSID, password, algorithm) {
-
+					return WifiWizard.formatWifiConfig(SSID, password, algorithm);
 				},
 
 				formatWPAConfig: function(SSID, password) {
-
+					return WifiWizard.formatWPAConfig(SSID, password);
 				},
 
-				addNetwork: function() {
+				addNetwork: function(wifi) {
+					var d = $q.defer();
+					WifiWizard.addNetwork(wifi,
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
 
+					return d.promise;
 				},
 
-				removeNetwork: function() {
+				removeNetwork: function(ssid) {
+					var d = $q.defer();
 
+					WifiWizard.removeNetwork(ssid,
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				},
 
-				connectNetwork: function() {
+				connectNetwork: function(ssid) {
+					var d = $q.defer();
 
+					WifiWizard.connectNetwork(ssid,
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				},
 
-				disconnectNetwork: function() {
+				disconnectNetwork: function(ssid) {
+					var d = $q.defer();
 
+					WifiWizard.disconnectNetwork(ssid,
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				},
 
 				listNetworks: function() {
+					var d = $q.defer();
 
+					WifiWizard.disconnectNetwork(
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				},
 
 				getScanResults: function() {
+					var d = $q.defer();
 
+					WifiWizard.getScanResults(
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				},
 
 				startScan: function() {
+					var d = $q.defer();
 
+					WifiWizard.startScan(
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				},
 
 				disconnect: function() {
+					var d = $q.defer();
 
+					WifiWizard.disconnect(
+						function(message) {
+							d.resolve(message);
+						}, function(error) {
+							d.reject(error);
+						}
+					);
+
+					return d.promise;
 				}
 			}
 		}])
